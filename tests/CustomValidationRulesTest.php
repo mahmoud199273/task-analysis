5ab839472d9680b2c7bba255191fedfac0a10790
mahmoud199273 <mahmoudabdelrahman73@gmail.com>
2019-12-30 22:35:38 +0200

    unit test

--- tests/CustomValidationRulesTest.php
+++ tests/CustomValidationRulesTest.php
@@ -2,8 +2,23 @@
 
 use Illuminate\Validation\Validator;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CustomValidationRulesTest extends TestCase
 {
 
+    public function testCheckTxt()
+    {
+        $rules = [
+            'txt_check' => 'required|max:255'
+        ];
+
+        $data = [
+            'txt_check' => "test"
+        ];
+
+        $v = $this->app['validator']->make($data, $rules);
+        $this->assertTrue($v->passes());
+    }
 }
